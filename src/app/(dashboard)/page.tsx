892f7c99cabbc78071a035a6f65c19369160f209
pangqingzhu <pangqingzhu2024@gmail.com>
2026-03-07 09:43:18 +0800

    02: dashboard

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import { DashboardView } from "@/features/dashboard/views/dashboard-view";
+
+const Page = () => {
+    return (
+        <DashboardView />
+    );
+};
+
+export default Page;
---
+++ src/components/page-header.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { Headphones, ThumbsUp } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+import { cn } from "@/lib/utils";
+
+interface Props {
+    title: string;
+    className?: string;
+};
+
+export const PageHeader = ({ title, className }: Props) => {
+    return (
+        <div
+            className={cn(
+                "flex items-center justify-between border-b px-4 py-4",
+                className,
+            )}
+        >
+            <div className="flex items-center gap-2">
+                <SidebarTrigger />
+                <h1 className="text-lg font-semibold tracking-tight">{title}</h1>
+            </div>
+            <div className="flex items-center gap-3">
+                <Button variant="outline" size="sm" asChild>
+                    <Link href="mailto:business@codewithantonio.com">
+                        <ThumbsUp />
+                        <span className="hidden lg:block">Feedback</span>
+                    </Link>
+                </Button>
+                <Button variant="outline" size="sm" asChild>
+                    <Link href="mailto:business@codewithantonio.com">
+                        <Headphones />
+                        <span className="hidden lg:block">Need help?</span>
+                    </Link>
+                </Button>
+            </div>
+        </div>
+    );
+};
